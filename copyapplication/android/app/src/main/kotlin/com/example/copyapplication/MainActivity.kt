2ce7c95024cf4e7a13a73a718506b2066f5a0639
soyunim <togksk123@naver.com>
2023-08-30 01:02:11 +0900

    create toyproject: copyApp

---
+++ copyapplication/android/app/src/main/kotlin/com/example/copyapplication/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.copyapplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ copyapplication/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hi'),
+        ),
+        body: Center(
+          child: Text('hello'),
+        ),
+      ),
+    );
+  }
+}
