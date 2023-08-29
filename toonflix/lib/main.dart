065c6fb90ca897547f5c5519906a489bb6e8cc2c
soyunim <togksk123@naver.com>
2023-08-30 00:35:17 +0900

    root: material

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return MaterialApp(
       home: Text("Hello World!"),
     );
   }
