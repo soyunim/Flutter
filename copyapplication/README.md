f61af464bcd70a88da6bafa286a534ef45c8eb2e
soyunim <togksk123@naver.com>
2023-08-30 20:36:27 +0900

    new card

--- copyapplication/README.md
+++ copyapplication/README.md
@@ -14,3 +14,11 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+## Copy App
+
+| | Original App | Copy App |
+|:-:|:-:|:-:|
+|Image|![Alt text](image.png)| |
+|출처|https://dribbble.com/shots/19858341-Financil-Mobile-iOS-App| |
--- copyapplication/lib/main.dart
+++ copyapplication/lib/main.dart
@@ -66,7 +66,7 @@ class App extends StatelessWidget {
                 height: 10,
               ),
               const Text(
-                '\$5,194,481',
+                '\$5 194 481',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 50,
@@ -88,6 +88,84 @@ class App extends StatelessWidget {
                       bgColor: Color(0xFF1f2123),
                       textColor: Colors.white),
                 ],
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Wallets',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    'View All',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white.withOpacity(0.8),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFF1f2123),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Euro',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              const Text(
+                                '6 428',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                'EUR',
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.8),
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      //icon
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
