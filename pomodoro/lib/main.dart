4fe9de5c1b9a4f4a00e9e0224f69024699c02f5f
soyunim <togksk123@naver.com>
2023-08-31 02:31:00 +0900

    create pomodoro

---
+++ pomodoro/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
