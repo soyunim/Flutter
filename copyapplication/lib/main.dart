de52933ed7127d0310154ec5069a6dd712c0800b
soyunim <togksk123@naver.com>
2023-08-30 19:49:36 +0900

    new class: button

--- copyapplication/lib/main.dart
+++ copyapplication/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:copyapplication/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -75,46 +76,17 @@ class App extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFF2B33A),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 20,
-                        horizontal: 45,
-                      ),
-                      child: Text(
-                        'Transfer',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF1f2123),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 20,
-                        horizontal: 45,
-                      ),
-                      child: Text(
-                        'Request',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
+                  Button(
+                      text: "Transfer",
+                      bgColor: Color(0xFFF2B33A),
+                      textColor: Colors.black),
+                  Button(
+                      text: "Request",
+                      bgColor: Color(0xFF1f2123),
+                      textColor: Colors.white),
                 ],
               ),
             ],
