81bfec032da0b789ab5c7462914fba7ea7ddbc3a
soyunim <togksk123@naver.com>
2023-08-31 18:04:20 +0900

    feat: pause timer

--- pomodoro/lib/screens/home_screen.dart
+++ pomodoro/lib/screens/home_screen.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = 1500;
+  bool isRunning = false;
   late Timer timer;
 
   void onTick(Timer timer) {
@@ -24,6 +25,16 @@ class _HomeScreenState extends State<HomeScreen> {
       const Duration(seconds: 1),
       onTick,
     );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -53,8 +64,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: IconButton(
                   iconSize: 100,
                   color: Theme.of(context).cardColor,
-                  onPressed: onStartPressed,
-                  icon: const Icon(Icons.play_arrow_rounded),
+                  onPressed: isRunning ? onPausePressed : onStartPressed,
+                  icon: Icon(isRunning
+                      ? Icons.pause_circle_outline_rounded
+                      : Icons.play_arrow_rounded),
                 ),
               ),
             ),
